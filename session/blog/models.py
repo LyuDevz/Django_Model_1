f16775d99f07710befc654b90b3ba7ff040f3768
LyuDevz <lyuii0726@gmail.com>
2023-03-27 00:57:10 +0900

    final push

--- session/blog/models.py
+++ session/blog/models.py
@@ -4,7 +4,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=100, help_text='블로그 제목을 작성해주세요')
     author = models.CharField(max_length=10,default="")
     StudentID = models.CharField(max_length=8,default="", help_text="학번을 입력해주세요(예시:20181020)")
-
+    email = models.EmailField(max_length=128, null=True)
     posted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
