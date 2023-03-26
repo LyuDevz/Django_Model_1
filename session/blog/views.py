efea8166130f692bcb517a19db6fb7583dc7950f
LyuDevz <lyuii0726@gmail.com>
2023-03-26 23:27:37 +0900

    first commit

---
+++ session/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Blog
+from django.http import HttpResponse
+
+def blog(request):
+    blog = Blog.objects.all()
+    return HttpResponse(blog)
---
+++ session/session/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from blog import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls'))
+]
+
+
+# 127.0.0.1:8000 기본 url
+# 127.0.0.1:8000/admin/
